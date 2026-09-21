47bc435c6fcfa0cb4f5bc000873c0c399977603c
NinaTrofimov <ntrofimov1@my.waketech.edu>
2026-09-21 10:30:33 -0400

    Cleaned up errors and added methods

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,12 @@
 class Bank:
     title = "Bank"
 
-    def __init__(self, customer_name: str, current_balance: float, minimum_balance: float):
+    def __init__(self, customer_name: str, current_balance: float, minimum_balance: float, account_number:int, routing_number:int):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+        self.account_number = account_number
+        self.routing_number = routing_number
 
     def deposit(self):
         amount = float(input("Enter amount to deposit: "))
@@ -14,7 +16,7 @@ class Bank:
     def withdraw(self):
         amount = float(input("Enter amount to withdraw: "))
 
-        if amount > self.current_balance:
+        if self.current_balance - amount < self.minimum_balance:
             print("Amount exceeds current balance")
         else:
             self.current_balance -= amount
---
+++ main.py
@@ -0,0 +1,3 @@
+from checking_account import Checking
+from routing_account import Savings
+
